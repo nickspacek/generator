3fbe7ffacd19285c256916278246bd208326c2ad
Michael Davis <michael@damaru.com>
2019-11-25 09:34:59 -0500

    Producer mostly working.

--- templates/solace-lib/.filters/all.js
+++ templates/solace-lib/.filters/all.js
@@ -4,17 +4,19 @@ module.exports = ({ Nunjucks, _ }) => {
   typeMap.set('integer', 'int')
   typeMap.set('string', 'String')
   
-  var formats = new Map();
-  formats.set('string', '%s');
-  formats.set('enum', '%s');
-  formats.set('integer', '%d');
+  var formatMap = new Map();
+  formatMap.set('string', '%s');
+  formatMap.set('enum', '%s');
+  formatMap.set('integer', '%d');
 
   Nunjucks.addFilter('topicInfo', ([channelName, channel]) => {
     var ret = {};
     var publishTopic = String(channelName);
     var subscribeTopic = String(channelName);
-
     var params = [];
+    var functionParamList = "";
+    var functionArgList = "";
+    var first = true;
 
     console.log("params: " + JSON.stringify(channel.parameters()));
     for (var name in channel.parameters()) {
@@ -22,68 +24,50 @@ module.exports = ({ Nunjucks, _ }) => {
         var schema = channel.parameter(name)['_json']['schema']
         console.log("schema: " + dump(schema))
         var type = schema.type
-        var param = { "name" : _.upperFirst(name) }
+        var param = { "name" : _.lowerFirst(name)  }
+
+        if (first) {
+            first = false
+        } else {
+            functionParamList += ", "
+            functionArgList += ", "
+        }
 
         if (type) {
             console.log("It's a type: " + type)
-            param.type = type
-            var printfArg = formats.get(type)
+            var javaType = typeMap.get(type)
+            if (!javaType) throw new Error("topicInfo filter: type not found in typeMap: " + type);
+            param.type = javaType
+            var printfArg = formatMap.get(type)
             console.log("printf: " + printfArg)
-            if (!printfArg) throw new Error("topicInfo filter: Unknown type: " + type)
-            console.log("Replacing " + nameWithBrackets);
-            publishTopic = publishTopic.replace(nameWithBrackets, printfArg);
+            if (!printfArg) throw new Error("topicInfo filter: type not found in formatMap: " + type)
+            console.log("Replacing " + nameWithBrackets)
+            publishTopic = publishTopic.replace(nameWithBrackets, printfArg)
         } else {
-            var en = schema.enum;
+            var en = schema.enum
             if (en) {
                 console.log("It's an enum: " + en)
-                param.type = "enum"
+                param.type = _.upperFirst(name)
                 param.enum = en;
-                console.log("Replacing " + nameWithBrackets);
-                publishTopic = publishTopic.replace(nameWithBrackets, "%s");
+                console.log("Replacing " + nameWithBrackets)
+                publishTopic = publishTopic.replace(nameWithBrackets, "%s")
             } else {
                 throw new Error("topicInfo filter: Unknown parameter type: " + JSON.stringify(schema))
             }
         }
 
-        subscribeTopic = subscribeTopic.replace(nameWithBrackets, "*");
+        subscribeTopic = subscribeTopic.replace(nameWithBrackets, "*")
+        functionParamList += param.type + " " + param.name
+        functionArgList += param.name
         params.push(param)
     }
+    ret.functionArgList = functionArgList;
+    ret.functionParamList = functionParamList;
     ret.channelName = channelName;
     ret.params = params;
     ret.publishTopic = publishTopic;
     ret.subscribeTopic = subscribeTopic;
     return ret;
-/*
-var topic = "solace/orders/{verb}/1/{trace}/{span}";
-
-
-var params = [
-    { 'name': 'verb', 'type': 'enum' },
-    { 'name': 'trace', 'type': 'string' },
-    { 'name': 'span', 'type': 'integer' },
-    ];
-
-foo = processTopic(topic, params);
-console.log(foo);
-
-function processTopic(topic, params) {
-    var parts = topic.split('/');
-
-    for (var i in params) {
-        var param = params[i];
-        var name = "{" + param.name + "}";
-        var format = formats.get(param.type);
-        console.log("param: " + name + " " + format);
-        topic = topic.replace(name, format);
-    }
-
-    ret.params = params;
-    ret.topic = topic;
-    return ret;
-}
-*/
-
-
   });
 
   Nunjucks.addFilter('fixType', (str) => {
